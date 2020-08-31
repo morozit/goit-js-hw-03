ab1713db4c61a9b5ff380865640bcdbc1863da3b
Roman <65686226+morozit@users.noreply.github.com>
2020-08-31 16:57:53 +0300

    Create task-3-0.js

---
+++ js/task-3-0.js
@@ -0,0 +1,16 @@
+//! Добавлення властивостей в обєкт
+
+const exampleObject = {
+  name: "Приклад властивості обєекта, як рядок",
+  arrayNumbers: ["Число-1", "Число-2", "Число-3"],
+};
+console.log(exampleObject);
+
+exampleObject.exampleAddPropertis = "Приклад добавлення властивості в Обєкт";
+console.log(exampleObject);
+
+//! Переоприділення властивостей в обєкті
+
+exampleObject.exampleAddPropertis =
+  // "Приклад переоприділення властивості в Обєкті";
+  console.log(exampleObject);
