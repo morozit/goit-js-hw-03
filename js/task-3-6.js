e35380ff49cdfac453f4b0b1a94b04c242c1b422
Roman <65686226+morozit@users.noreply.github.com>
2020-09-01 15:02:10 +0300

    Update task-3-6.js

--- js/task-3-6.js
+++ js/task-3-6.js
@@ -1,3 +1,6 @@
+// TODO: variant-1
+
+/*
 function calculateTotalPrice(array, prop) {
   "use strict";
   // Write code under this line
@@ -8,6 +11,44 @@ function calculateTotalPrice(array, prop) {
         let { price, quantity } = item;
         result += price * quantity;
       }
+    }
+  }
+  return result;
+}
+
+// Объекты и ожидаемый результат
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Радар", price: 1280, quantity: 2 },
+  { name: "Радар", price: 1320, quantity: 1 },
+  { name: "Сканер", price: 2700, quantity: 1 },
+  { name: "Сканер", price: 2500, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
+
+console.log(calculateTotalPrice(products, "Радар"));
+// 9080
+
+console.log(calculateTotalPrice(products, "Сканер"));
+// 10200
+
+console.log(calculateTotalPrice(products, "Захват"));
+// 2400
+
+console.log(calculateTotalPrice(products, "Дроид"));
+// 2800
+*/
+
+// TODO: variant-2
+
+function calculateTotalPrice(array, prop) {
+  "use strict";
+  // Write code under this line
+  let result = 0;
+  for (const item of array) {
+    if (prop === item.name) {
+      result += item.price * item.quantity;
     }
   }
   return result;
